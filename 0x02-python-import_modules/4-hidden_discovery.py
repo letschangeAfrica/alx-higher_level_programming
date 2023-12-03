49ef1ea8d3f8512e5f4333bbfc35cacd25f32910
letschangeAfrica <ismaelkagou@gmail.com>
2023-12-02 23:53:23 -0800

    I should not put the

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     import hidden_4
     d = dir
-    for i in the range(0, len(d)):
+    for i in range(0, len(d)):
         if d[i][0:2] != "__":
             print("{}".format(d[i]))
